a8a2e01fd2b743d132c604da7447a9d6f9a0312c
Yoanm <yoanm@users.noreply.github.com>
2023-01-25 07:35:05 +0100

    move todo

--- main.go
+++ main.go
@@ -9,40 +9,6 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
-// @TODO configTemplate: tpl => configTemplates: [tpl1, tpl2]
-// @TODO
-//  - Write test for core/terraform_loader.go
-//  - Use the command to create repository for tfsig
-//  - Push tfsig to github and check coverage miss from codecov.io
-//  - Use the command to create repository for gh2tf
-//  - Push gh2tf to github and check coverage miss from codecov.io
-//  - Use the command to create repository for github-tf
-//  - Push github-tf to github and check coverage miss from codecov.io
-//  - then starts pushing others lib to github
-// @TODO PUSH ON GITHUB + CREATE RELEASE + START USING IT
-// LATER
-// @TODO github action
-// See https://docs.github.com/en/actions/security-guides/encrypted-secrets#storing-large-secrets
-// See https://notes.nishkal.in/snippets/github-actions.html
-// See https://itsfoss.com/gpg-encrypt-files-basic/
-//  - Decrypt terraform.tfstate with a GPG key stored as secret (or symetric encryption ??) + password stored as secret
-//  - terraform init
-//  - terraform plan
-//  - !! prevent updates on current repository
-//  - For PR only: output as PR comment
-//  - For manual trigger only *and only if my user (check if doable to use an id instead of a name)*:
-//    - terraform apply
-//    - encrypt terraform.tfstate to terraform.tfstate.gpg (exclude terraform.tfstate + terraform.tfstate* files and .terraform directory with .gitignore)
-//    - git add terraform.tfstate.gpg + git commit -m "State update following XXXX commit" + git push
-// @TODO re-read github_repository + github_branch_protection doc and manage missing field
-// @TODO enhance json-schema (enum, min/max, etc)
-// LATER LATER
-// @TODO manage github_user_ssh_key and github_user_gpg_key
-// @TODO manage github_repository_file (to add required common file on every repo)
-// @TODO manage github action secrets
-// LATER LATER MAYBE
-// @TODO manage organization (user role, team + membership)
-
 // Build time variables.
 var (
 	version = "dev"
